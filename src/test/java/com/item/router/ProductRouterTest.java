8243e536d3aa859625311962c078f28148bbb358
SANAYEE1996 <dudtkd0219@gmail.com>
2023-11-28 11:39:46 +0900

    test는 어떻게 해야할지 모르겠다.. 일단 postman 으로는 다 통과됨

--- src/test/java/com/item/router/ProductRouterTest.java
+++ src/test/java/com/item/router/ProductRouterTest.java
@@ -2,34 +2,50 @@ package com.item.router;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.item.dto.SearchRequestDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-@SpringBootTest
-@AutoConfigureWebTestClient
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class ProductRouterTest {
 
     @Autowired
-    private WebTestClient client;
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void init(){
+    }
+
+
     @DisplayName("상품 디테일 조회")
     @Test
     public void getProductDetailTest() throws Exception {
-        client.get().uri("/product/detail/product_3").exchange();
+
+
     }
 
     @DisplayName("상품 검색 테스트")
     @Test
     public void getProductSearchTest() throws Exception {
-        SearchRequestDto searchRequestDto = new SearchRequestDto("나이키", 0, 10);
+        SearchRequestDto searchRequestDto = new SearchRequestDto("셔츠", 0, 10);
         String content = objectMapper.writeValueAsString(searchRequestDto);
+
+        mockMvc.perform(post("/product/search").contentType(MediaType.APPLICATION_JSON).content(content))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
     }
 }
